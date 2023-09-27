e5ff5619b49e48188fc7a5d5ca01d27771e6b598
Arn <arn.arn3328@gmail.com>
2023-09-27 15:03:35 +0700

    fix(cd): add minio env

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -2,16 +2,21 @@ import {
   Controller,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { BufferedFile } from '@/minio-client/file.model';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtGuard } from '@/common/guards/jwt.guard';
 
 @Controller('upload')
 export class UploadController {
   constructor(private uploadService: UploadService) {}
 
+  @UseGuards(JwtGuard)
+  @ApiBearerAuth('JWT-auth')
   @Post()
   @UseInterceptors(FileInterceptor('image'))
   async uploadImage(@UploadedFile() image: BufferedFile) {
