77cdade63aabda687a8ae0ea1c75beb469145dc9
Arn <arn.arn3328@gmail.com>
2023-09-28 01:35:27 +0700

    chore(decorator): make decorator reuseable

--- src/decorators/api-image.decorator.ts
+++ src/decorators/api-image.decorator.ts
@@ -2,15 +2,19 @@ import { UseInterceptors, applyDecorators } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes } from '@nestjs/swagger';
 
-export function ApiImage() {
+export function ApiImage(
+    fieldName: string = 'file',
+    required: boolean = false,
+) {
   return applyDecorators(
-    UseInterceptors(FileInterceptor('image')),
+    UseInterceptors(FileInterceptor(fieldName)),
     ApiConsumes('multipart/form-data'),
     ApiBody({
       schema: {
         type: 'object',
+        required: required ? [fieldName]: [],
         properties: {
-          image: {
+          [fieldName]: {
             type: 'string',
             format: 'binary',
           },
