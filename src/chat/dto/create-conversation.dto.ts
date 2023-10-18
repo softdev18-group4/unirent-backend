211e66d2a04a2479a53d0daec96a33ae5ea0a875
Arn <arn.arn3328@gmail.com>
2023-10-18 09:57:43 +0700

    chore(chat): api property for chat dto

--- src/chat/dto/create-conversation.dto.ts
+++ src/chat/dto/create-conversation.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateConversationDto {
+  @ApiProperty()
   @IsNotEmpty()
   @IsString()
   userId: string;
